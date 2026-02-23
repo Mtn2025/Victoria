dc2774dc7109cad9ab2377e1027c0fcec0d2142b
Martin <martincalderon@gmail.com>
2026-02-23 02:12:27 -0500

    fix: recreate missing alembic target revision for seamless upgrade

--- backend/alembic/versions/45dabae7ee5d_add_language_column_to_agents.py
+++ backend/alembic/versions/45dabae7ee5d_add_language_column_to_agents.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '45dabae7ee5d'
-down_revision: Union[str, Sequence[str], None] = None
+down_revision: Union[str, None] = 'b2c3d4e5f6a7'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
